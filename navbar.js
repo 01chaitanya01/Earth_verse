ce83ffcdceeb21a48b457dbf13126d69eb72b248
chaitanya choudhari <100710855+01chaitanya01@users.noreply.github.com>
2023-03-09 23:24:31 +0530

    uploading all files

---
+++ navbar.js
@@ -0,0 +1,11 @@
+var subMenu = document.getElementById("subMenu");
+
+function showMenu(){
+if(subMenu.style.height ==="0vh"){
+  subMenu.style.height = "25vh";
+}
+else{
+  subMenu.style.height = "0vh"
+}
+}
+
